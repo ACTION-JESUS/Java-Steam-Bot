767e2220b02a3671ae1ce1817ec5aefee8f44240
ACTION JESUS <action_jesus@steampowered.com>
2015-08-30 14:52:13 -0700

    Updated "about" info

--- src/main/java/eu/jhomlala/steambot/configuration/SteamBotConfiguration.java
+++ src/main/java/eu/jhomlala/steambot/configuration/SteamBotConfiguration.java
@@ -2,8 +2,8 @@ package eu.jhomlala.steambot.configuration;
 
 public class SteamBotConfiguration {
 
-	public static final String currentBotVersion = "0.0.1";
-	public static final String authorName = "Jakub Homlala";
+	public static final String currentBotVersion = "0.9";
+	public static final String authorName = "ACTION JESUS";
 	public static final String relaseDate = "August 2015";
 }
  
