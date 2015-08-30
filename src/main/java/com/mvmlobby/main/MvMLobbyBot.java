eaaccde9090238b14901fa8c285ac227c3107e3d
ACTION JESUS <action_jesus@steampowered.com>
2015-08-30 14:51:02 -0700

    New class to configure and start the bot

---
+++ src/main/java/com/mvmlobby/main/MvMLobbyBot.java
@@ -0,0 +1,34 @@
+package com.mvmlobby.main;
+
+import eu.jhomlala.steambot.configuration.BotConfiguration;
+import eu.jhomlala.steambot.exceptions.InvalidSteamBotConfigurationException;
+import eu.jhomlala.steambot.main.SteamBot;
+
+
+public class MvMLobbyBot {
+	   
+	public static void main(String[] args) {
+
+		MvMLobbyConfig lobbyConfig = MvMLobbyConfig.getInstance();
+
+		BotConfiguration.Builder builder = new BotConfiguration.Builder()
+								.setBotname(lobbyConfig.getBotname())
+								.setUsername(lobbyConfig.getSteamUserId())
+								.setPassword(lobbyConfig.getSteamPassword());
+		
+		if (lobbyConfig.getSteamGuardCode() != null && !lobbyConfig.getSteamGuardCode().equals("")) {
+			builder.setSteamGuardCode(lobbyConfig.getSteamGuardCode());
+		}
+		
+		BotConfiguration config = builder.build();
+		
+		try {
+			SteamBot steamBot = new SteamBot(config);
+			steamBot.start();
+		} catch (InvalidSteamBotConfigurationException e) {
+			System.out.println("SteamBot configuration is invalid");
+		}
+
+	}
+
+}
