89688373867d63865fa4197777c4a858961f16ca
ACTION JESUS <action_jesus@steampowered.com>
2015-09-07 17:10:41 -0700

    Fixed friendslist callbacks
    It now correctly adds friends, deletes friends, and accepts invitations
    correctly.

--- src/main/java/eu/jhomlala/steambot/controllers/FriendListController.java
+++ src/main/java/eu/jhomlala/steambot/controllers/FriendListController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import org.apache.log4j.Logger;
 
+import uk.co.thomasc.steamkit.steam3.handlers.steamfriends.SteamFriends;
 import uk.co.thomasc.steamkit.steam3.handlers.steamfriends.types.Friend;
 import uk.co.thomasc.steamkit.types.steamid.SteamID;
 import eu.jhomlala.steambot.utils.Log;
@@ -21,56 +22,94 @@ public class FriendListController {
 		log = Log.getInstance();
 	}
 	
-	public void updateFriendList(Set<Friend> friends)
+	public void updateFriendList(SteamFriends steamfriends, Set<Friend> friends)
 	{
-		int updateActions = 0;
-		int addActions = 0;
 		lastUpdate = new Date();
+		String relationship = "";
 		for (Friend friend: friends)
 		{
-			if (isFriendInList(friend.getSteamId()))
-			{
-				updateFriend(friend);
-				updateActions++;
-			}
-			else
-			{
+			
+			switch (friend.getRelationship()) {
+			case None:
+				// Player removed this bot as a friend
+				relationship = "None";
+				deleteFriend(friend);
+				break;
+				
+			case Blocked:
+				// Player blocked or removed the friend invitation
+				relationship = "Blocked";
+				steamfriends.removeFriend(friend.getSteamId());
+				deleteFriend(friend);
+				break;
+				
+			case PendingInvitee:
+				relationship = "PendingInvitee";
+				break;
+				
+			case RequestRecipient:
+				// Player sent a friend invitation.  Adding the friend will trigger the "Friend" relationship on success.
+				relationship = "RequestRecipient";
+				steamfriends.addFriend(friend.getSteamId());
+				break;
+				
+			case RequestInitiator:
+				relationship = "RequestInitiator";
+				break;
+				
+			case PendingInviter:
+				relationship = "PendingInviter";
+				break;
+				
+			case Friend:
+				// Friend relationship confirmed
+				relationship = "Friend";
 				addFriend(friend);
-				addActions++;
+				break;
+				
+			case Ignored:
+				relationship = "Ignored";
+				break;
+				
+			case IgnoredFriend:
+				relationship = "IgnoredFriend";
+				break;
+				
+			case SuggestedFriend:
+				relationship = "SuggestedFriend";
+				break;
+				
 			}
+			log.info("Player " + friend.getSteamId().toString() + " changed the relationship to " + relationship);
 		}
-		log.info("Friendlist update: Update actions: " +updateActions+" Add Actions: "+addActions);
-		log.info("Friendlist update date: "+lastUpdate);
 	}
 	
-	public void addFriend(Friend friend)
+	public synchronized void addFriend(Friend friend)
 	{
-		log.info(friend.getSteamId());
 		friendList.add(friend);
+		log.info("Added friend : " + friend.getSteamId());
 	}
 	
-	public void updateFriend(Friend friend)
+	public void deleteFriend(Friend friend)
 	{
-		for (Friend friendFromList: friendList)
-		{
-			if (friendFromList.getSteamId().getAccountID() == friend.getSteamId().getAccountID())
-			{
-				friendList.remove(friendFromList);
-				friendList.add(friend);
-			}
+		Friend friendToDelete = getFriendInList(friend.getSteamId());
+		
+		if (friendToDelete != null) {
+			friendList.remove(friendToDelete);
+			log.info("Deleted friend " + friend.getSteamId());
 		}
 	}
 
-	public boolean isFriendInList(SteamID steamID)
+	public Friend getFriendInList(SteamID steamID)
 	{
 		for (Friend friend: friendList)
 		{
 			if (friend.getSteamId().getAccountID() == steamID.getAccountID())
 			{
-				return true;
+				return friend;
 			}
 		}
-		return false;
+		return null;
 	}
 	public List<Friend> getFriendList() {
 		return friendList;
