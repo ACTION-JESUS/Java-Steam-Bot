85edc4b2d47f52873565197c015d7fbd8d66a37d
ACTION JESUS <action_jesus@steampowered.com>
2015-09-13 12:35:24 -0700

    Simplified configuration

--- src/main/java/com/mvmlobby/main/MvMLobbyBot.java
+++ src/main/java/com/mvmlobby/main/MvMLobbyBot.java
@@ -1,7 +1,5 @@
 package com.mvmlobby.main;
 
-import eu.jhomlala.steambot.configuration.BotConfiguration;
-import eu.jhomlala.steambot.exceptions.InvalidSteamBotConfigurationException;
 import eu.jhomlala.steambot.main.SteamBot;
 
 
@@ -9,26 +7,8 @@ public class MvMLobbyBot {
 	   
 	public static void main(String[] args) {
 
-		MvMLobbyConfig lobbyConfig = MvMLobbyConfig.getInstance();
-
-		BotConfiguration.Builder builder = new BotConfiguration.Builder()
-								.setAcceptFriendRequest(true)
-								.setBotname(lobbyConfig.getBotname())
-								.setUsername(lobbyConfig.getSteamUserId())
-								.setPassword(lobbyConfig.getSteamPassword());
-		
-		if (lobbyConfig.getSteamGuardCode() != null && !lobbyConfig.getSteamGuardCode().equals("")) {
-			builder.setSteamGuardCode(lobbyConfig.getSteamGuardCode());
-		}
-		
-		BotConfiguration config = builder.build();
-		
-		try {
-			SteamBot steamBot = new SteamBot(config);
-			steamBot.start();
-		} catch (InvalidSteamBotConfigurationException e) {
-			System.out.println("SteamBot configuration is invalid");
-		}
+		SteamBot steamBot = new SteamBot();
+		steamBot.start();
 
 	}
 
