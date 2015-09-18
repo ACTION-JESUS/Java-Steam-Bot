fb67336577a59043cb0e41222dce69ac0f12938d
ACTION JESUS <action_jesus@steampowered.com>
2015-09-17 22:07:11 -0700

    Merged changes with the lobby bot

--- src/main/java/eu/jhomlala/steambot/controllers/FriendChatController.java
+++ src/main/java/eu/jhomlala/steambot/controllers/FriendChatController.java
@@ -1,5 +1,4 @@
 package eu.jhomlala.steambot.controllers;
-import eu.jhomlala.steambot.configuration.SteamBotConfiguration;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -7,6 +6,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import uk.co.thomasc.steamkit.steam3.handlers.steamfriends.callbacks.FriendMsgCallback;
+import eu.jhomlala.steambot.configuration.BotConfiguration;
 import eu.jhomlala.steambot.controllers.models.SteamChat;
 import eu.jhomlala.steambot.main.SteamBot;
 import eu.jhomlala.steambot.utils.Log;
@@ -22,8 +22,7 @@ public class FriendChatController {
 		super();
 		this.steamBot = steamBot;
 		this.messageCache = new ArrayList<SteamChat>();
-		this.messageCacheSize = steamBot.getBotConfiguration()
-				.getFriendMessagesCacheSize();
+		this.messageCacheSize = BotConfiguration.getInstance().getFriendMessagesCacheSize();
 		this.log = Log.getInstance();
 	}
 
@@ -53,11 +52,12 @@ public class FriendChatController {
 
 		if(callbackMessage.equals("about"))
 		{
+			BotConfiguration config = BotConfiguration.getInstance();
 			returnMessage = "\nJava SteamBot"
 					+"\n----------------------------------------"
-					+"\nVersion:" +SteamBotConfiguration.currentBotVersion
-					+"\nAuthor:" + SteamBotConfiguration.authorName
-					+"\nRelease date: "+SteamBotConfiguration.relaseDate
+					+"\nVersion:" +config.getVersion()
+					+"\nAuthor:" + config.getAuthor()
+					+"\nRelease date: "+config.getRelease_date()
 					+"\n----------------------------------------";
 			
 		}
